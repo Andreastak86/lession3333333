bf6e7b5fe9730d8b1e3617a2409befc1dc0dd48d
Andreastak86 <andreastak.kodehode@gmail.com>
2025-08-20 11:31:25 +0200

    add hi

--- script.js
+++ script.js
@@ -5,4 +5,13 @@ let myAge = 38;
 const isMale = true;
 var useThis = "Fyfy";
 
+function siHei() {
+    console.log("hei, hei");
+}
+siHei();
+siHei();
+siHei();
+siHei();
+siHei();
+
 console.log(myName + " " + myAge + ", Jeg er en mann");
